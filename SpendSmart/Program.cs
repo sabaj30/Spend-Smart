e585539b62a72a62159b0d1e35d9016f2c173770
saba jafari <sabajafaricc@gmail.com>
2025-06-05 18:29:41 +0330

    Spend Smart MVC web app (in-memory CRUD)

---
+++ SpendSmart/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using SpendSmart.Models;
+
+namespace SpendSmart
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<SpendSmartDbContext>(options =>
+            options.UseInMemoryDatabase("SpendSmartDb"));
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
